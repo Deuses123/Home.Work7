486db2cb6307353c7964bdc78120142ce008ac99
Deuses123 <deusdeqds@gmail.com>
2023-10-23 23:55:01 +0600

    first commit

---
+++ MyClassLib/WorldOfTanks/Tank.cs
@@ -0,0 +1,29 @@
+﻿namespace MyClassLib.WorldOfTanks;
+
+public class Tank
+{
+    public string Model { get; set; 
+    public int? Ammunition { get; set; }
+    public int? ArmorLevel { get; set; }     
+    public int? LevelOfManeuverability { get; set; }
+
+    public Tank(string model, int ammunition, int armorLevel, int levelOfManeuverability)
+    {
+        Model = model;
+        if (ammunition >= 0 && ammunition <= 100)
+            Ammunition = ammunition;
+            else throw new Exception("Не правильное количество боекомплекта должно быть 0, 100");
+        checkParam(armorLevel, " уровня брони ");
+        checkParam(levelOfManeuverability, " уровня маневренности ");
+    }
+
+
+    public override string ToString()
+    {
+        return
+                           "\nМодель: " + Model
+                         + "\nБоекомплект: " + Ammunition + " снарядов "
+                         + "\nУровень брони: " + ArmorLevel
+                         + "\nУровень маневренности: " + LevelOfManeuverability + "\n";
+    }
+}
